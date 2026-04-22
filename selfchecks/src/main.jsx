411c7aead74eb09e4cf73c70962f6a07b7f9ce14
DontsaClaudia <donstsaclaudia@gmail.com>
2026-04-22 10:38:21 +0200

    creation des fichier de la structure et des routes

--- selfchecks/src/main.jsx
+++ selfchecks/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// ReactDOM.createRoot : crée l'application React dans la div #root de index.html
+// BrowserRouter : active le système de routing (navigation entre pages)
+// basename : préfixe de toutes les URLs (utile pour GitHub Pages)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* StrictMode : détecte les problèmes potentiels en développement */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
