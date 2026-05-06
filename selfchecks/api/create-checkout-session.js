97e684c9c5e95db42baa1db6fc9bb8912de2242a
DontsaClaudia <donstsaclaudia@gmail.com>
2026-05-06 15:10:39 +0200

    erreur stripe

--- selfchecks/api/create-checkout-session.js
+++ selfchecks/api/create-checkout-session.js
@@ -1,14 +1,11 @@
-import Stripe from 'stripe'
-
+const Stripe = require('stripe')
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
-
   const { priceId, offre } = req.body
-
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
@@ -18,7 +15,6 @@ export default async function handler(req, res) {
       success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.headers.origin}/offres`,
     })
-
     res.status(200).json({ sessionId: session.id })
   } catch (error) {
     res.status(500).json({ error: error.message })
--- selfchecks/api/get-session.js
+++ selfchecks/api/get-session.js
@@ -1,24 +1,20 @@
-import Stripe from 'stripe'
-import crypto from 'crypto'
-
+const Stripe = require('stripe')
+const crypto = require('crypto')
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const { session_id } = req.query
-
   try {
     const session = await stripe.checkout.sessions.retrieve(session_id)
-
     const token = crypto
       .createHash('sha256')
       .update(`${session.customer_email}${session.id}`)
       .digest('hex')
       .substring(0, 16)
       .toUpperCase()
-
     res.status(200).json({
       email: session.customer_email,
-      token: token,
+      token,
       offre: session.metadata?.offre,
     })
   } catch (error) {
